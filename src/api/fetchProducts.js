356a9926336df8b70080cbf21b1a6251308fb856
maanik sharma <manikss123456@gmail.com>
2024-01-11 18:36:40 +0530

    bug fix

--- src/api/fetchProducts.js
+++ src/api/fetchProducts.js
@@ -1,7 +1,9 @@
 const fetchProducts = async ({ queryKey }) => {
   const page = queryKey[1];
   console.log(page);
-  return await fetch(`http://localhost:3000/api/products?pagecount=${page}`)
+  return await fetch(
+    `${import.meta.env.VITE_SERVERURL}/api/products?pagecount=${page}`
+  )
     .then((res) => res.json())
     .then((res) => {
       return res;
